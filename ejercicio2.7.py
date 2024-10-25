a507c760850503c30e2e33d85d9b3f0f6a08501c
DanielMafla320 <daniel.mafla@gmail.com>
2024-10-25 12:51:30 -0500

    second commit

--- ejercicio2.7.py
+++
@@ -1,6 +0,0 @@
-galones_litros = 3785
-galones_leche = int(input("Ingrese la cantidad de galones: "))
-litros_leche = galones_leche * galones_litros
-print(f"El total de litros de leche es: {litros_leche:,}")
-
-
--- ejercicio2.9.py
+++
@@ -1,7 +0,0 @@
-#Pago semanal por trabajador
-horas_trabajadas_por_dia = int(input("Ingresa las horas trabajadas: "))
-costo_hora = int(input("Ingresa el costo por hora: "))
-horas_semanal = (horas_trabajadas_por_dia * 5)
-costo_semanal = (horas_semanal * costo_hora)
-print(f"El pago semanal es: {costo_semanal:,}")
-
